1dc17c0da86ee61c39b097876a23129261205546
jeet2610 <63009294+jeet2610@users.noreply.github.com>
2023-07-10 11:14:09 +0530

    Create helloworld.cu

---
+++ helloworld.cu
@@ -0,0 +1,9 @@
+// Uncomment this if using Google Colab %%writefile hello_world.cu
+
+#include <cuda.h>
+#include <iostream>
+#include <stdio.h>
+int main(void){
+  printf("Hello Shyam");
+  return 0;
+}
